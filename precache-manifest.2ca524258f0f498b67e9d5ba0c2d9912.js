a6f2007e26983c29494b7fd1f244db9539450c73
Connie <connie@applecrateseo.com>
2020-04-30 11:12:19 +0800

    Updates

---
+++ precache-manifest.2ca524258f0f498b67e9d5ba0c2d9912.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02224c4eb7fede321e0b9ee13b9314a4",
+    "url": "/react_practice1/index.html"
+  },
+  {
+    "revision": "b6ccdc77fd3efe93f5bd",
+    "url": "/react_practice1/static/css/main.581a6202.chunk.css"
+  },
+  {
+    "revision": "e6aeede4d7efe2d22802",
+    "url": "/react_practice1/static/js/2.6f8a2205.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react_practice1/static/js/2.6f8a2205.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6ccdc77fd3efe93f5bd",
+    "url": "/react_practice1/static/js/main.73ebf8f4.chunk.js"
+  },
+  {
+    "revision": "6b2f8eb930f4da1ec5fc",
+    "url": "/react_practice1/static/js/runtime-main.21d0eb49.js"
+  }
+]);
